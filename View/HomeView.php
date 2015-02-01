11916f7a65fd29ae87d904fc77971d592d219ac6
antoine <antoine@AN-Laptop>
2015-02-01 19:21:37 +0100

    Continuité du petit test, affiche l'ensemble des documents (plus de manière brute).

--- View/HomeView.php
+++ View/HomeView.php
@@ -10,13 +10,30 @@ class HomeView {
     public function defaultView() {
         include 'Content/header.php';
         include 'Content/home.html';
-        Self::documentsView();
+        Self::documentsAllView();
         include 'Content/footer.html';
     }
 
-    public function documentsView() {
+    /**
+     * Affiche tous les documents
+     */
+    public function documentsAllView() {
         $documents = Document::findAll();
-        print_r($documents);
+        foreach ($documents as $document) {
+            Self::documentView($document);
+        }
+    }
+    
+    /**
+     * Affiche un document donné en paramètre
+     * @param Document $document Document à afficher
+     */
+    public function documentView($document) {
+        echo "<div id=\"document\">";
+        echo "Titre : " . $document->nomD . "<br>";
+        echo "Description : " . $document->descD . "<br>";
+        echo "Lien : <a href=\"" . $document->urlD . "\">ici</a><br>";
+        echo "</div>";
     }
     
 }
