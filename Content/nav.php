44ecfa8b0589299b71b24490ebac5309ea149808
antoine <antoine@AN-Laptop>
2015-02-08 02:19:10 +0100

    module le menu en fonction du type d'utilisateur

--- Content/nav.php
+++ Content/nav.php
@@ -19,6 +19,14 @@ Menu avec tous les types de documents
         }
         ?>
     </nav>
+    <?php
+    if(in_array("1", $_SESSION['usertypes'])) {
+        echo "<br>";
+        echo "<a href=\"\">Coordonnées Conducteurs</a>";
+        echo "<br>";
+        echo "<a href=\"\">Comptes Utilisateurs</a>";
+    }
+    ?>
     <br>
     <br>
     <a href="http://facebook.com/tractlux" target="_blank">Facebook</a>
