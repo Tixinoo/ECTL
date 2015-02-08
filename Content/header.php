468beb161205600e37f05c94dfeca72bdb383362
antoine <antoine@AN-Laptop>
2015-02-08 16:10:28 +0100

    intègre un nouveau style pour la connexion (bootstrap)

--- Content/header.php
+++ Content/header.php
@@ -1,33 +1,26 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="author" content="ECTL-Team" />
-        <title>E-C-T-L</title>
-        <link rel="stylesheet" href="Lib/bootstrap-3.3.2-dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="Style/main_style.css" />
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="Lib/bootstrap-3.3.2-dist/js/bootstrap.min.js"></script>
-    </head>
+    <?php
+    include_once 'Content/head.html';
+    ?>
 
     <body>
-        
+
         <?php
         include_once 'Content/connecting_bar.php';
         ?>
-        
+
         <br>
 
         <?php
         include_once 'Content/nav.php';
         ?>
-        
+
         <br>
-        
+
         <div id="logoECTL">
             <h1>E-C-T-L</h1>
             <i>Espace Collaborateur TRACTLUX</i>
         </div>
-        
+
         <br>
--- login.php
+++ login.php
@@ -1,40 +1,34 @@
-<div>
-    <?php 
-
-    include_once 'Model/TypeU.php';
+<!DOCTYPE html>
+<html lang="fr">
     
-    session_start();
+<?php
+include_once 'Model/TypeU.php';
 
-    // Barre de connexion
+session_start();
 
-    // Si l'utilisateur est connecté
-    if (isset($_SESSION['username'])) {
-        
-        // On affiche un message, son nom d'utilisateur et un bouton pour se déconnecter
-        echo "Vous êtes connectés en tant que " . $_SESSION['username'];
-        foreach ($_SESSION['usertypes'] as $idtypeu) {
-            echo " (";
-            $TypeU = TypeU::findById($idtypeu);
-            echo $TypeU->nomTypeU;
-            echo ")";
-        }
-        echo "<form action=\"Controller/disconnection.php\" method=\"GET\">\n";
-        echo "<input type=\"submit\" value=\"Se déconnecter\"/>\n";
-        echo "</form>\n";
+include_once 'Content/head.html';
 
-    } else {
+// Si l'utilisateur est connecté
+if (isset($_SESSION['username'])) {
 
-        // Sinon, on affiche des champs et un bouton pour qu'il puisse se connecter
-        echo "<form action=\"Controller/connection.php\" method=\"POST\">\n";
-        echo "<input type=\"text\" name=\"username\" value=\"Tixinoo\"/>\n";
-        echo "<input type=\"password\" name=\"password\" value=\"toto\"/>\n";
-        echo "<input type=\"submit\" value=\"Se connecter\"/>\n";
-        echo "</form>\n";
-        
-        echo "<br>";
-        
-        echo "<a href=\"signin.php\">S'inscrire</a>";
-
+    // On affiche un message, son nom d'utilisateur et un bouton pour se déconnecter
+    echo "Vous êtes connectés en tant que " . $_SESSION['username'];
+    foreach ($_SESSION['usertypes'] as $idtypeu) {
+        echo " (";
+        $TypeU = TypeU::findById($idtypeu);
+        echo $TypeU->nomTypeU;
+        echo ")";
     }
-    ?>
-</div>
+    echo "<form action=\"Controller/disconnection.php\" method=\"GET\">\n";
+    echo "<input type=\"submit\" value=\"Se déconnecter\"/>\n";
+    echo "</form>\n";
+    
+} else {
+
+    // Sinon, on affiche des champs et un bouton pour qu'il puisse se connecter
+    include_once 'Content/login.html';
+    echo "<br>";
+    echo "<center><a href=\"signin.php\">S'inscrire</a></center>";
+    
+}
+?>
