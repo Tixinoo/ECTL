619344c6f4b8586ad0647d16c72a6f095ee62a88
antoine <antoine@AN-Laptop>
2015-02-08 17:06:22 +0100

    continue l'intégration du nouveau style grâce à bootstrap (2)

--- Content/header.php
+++ Content/header.php
@@ -11,23 +11,25 @@
         ?>
 
         <br>
-        
+
 
         <div class="container-fluid">
-        
-        <div class="row">
 
-        <?php
-        include_once 'Content/nav.php';
-        ?>
+            <div class="row">
 
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">  
-            
-        <br>
+                <?php
+                include_once 'Content/nav.php';
+                ?>
 
-        <div id="logoECTL">
-            <h1>E-C-T-L</h1>
-            <i>Espace Collaborateur TRACTLUX</i>
-        </div>
+                <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">  
 
-        <br>
+                    <br>
+
+                    <div id="logoECTL">
+                        <center>
+                            <h1>Espace Collaborateur</h1>
+                            <img width="400px" src="http://www.tractlux.com/images/design/logo.jpg">
+                        </center>
+                    </div>
+
+                    <br>
