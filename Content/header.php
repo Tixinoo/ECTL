404110ceaa58471848ca2531719486801b908254
antoine <antoine@AN-Laptop>
2015-02-08 12:39:59 +0100

    fait le lien avec bootstrap

--- Content/header.php
+++ Content/header.php
@@ -4,9 +4,11 @@
         <meta charset="utf-8">
         <meta name="author" content="ECTL-Team" />
         <title>E-C-T-L</title>
+        <link rel="stylesheet" href="Lib/bootstrap-3.3.2-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="Style/main_style.css" />
         <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="Lib/bootstrap-3.3.2-dist/js/bootstrap.min.js"></script>
     </head>
 
     <body>
