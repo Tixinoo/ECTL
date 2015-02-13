ee926b5f1f11e80217b16e512562ddd12b8d85a3
Antoine <Antoine@Antoine-Laptop>
2015-02-13 13:25:10 +0100

    intègre un nouveau style de bouton pour version mobile

--- Content/connecting_bar.php
+++ Content/connecting_bar.php
@@ -3,13 +3,13 @@
     <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container-fluid">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <button type="button" class="navbar-toggle collapsed btn btn-primary" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" aria-expanded="false" aria-controls="navbar">
+                <button type="button" class="navbar-toggle collapsed btn btn-info" data-toggle="collapse" data-target="#menu" aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
