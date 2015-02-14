372fd68185438a591ce02b35b2f6fff690b001ae
antoine <antoine@AN-Laptop>
2015-02-14 19:00:55 +0100

    intègre l'historique des publications

--- View/DocumentsView.php
+++ View/DocumentsView.php
@@ -22,6 +22,120 @@ class DocumentsView {
         include 'Content/footer.php';
     }
 
+    public static function publicationsView() {
+        $documents = Document::findAll();
+        echo "<div class=\"table-responsive\">";
+        echo "<table class=\"table table-striped table-hover\">";
+        echo "<thead>";
+            echo "<tr>";
+                echo "<th>";
+                echo "Date";
+                echo "</th>";
+                echo "<th>";
+                echo "Heure";
+                echo "</th>";
+                echo "<th>";
+                echo "Utilisateur";
+                echo "</th>";
+                echo "<th>";
+                echo "Document";
+                echo "</th>";
+                echo "<th>";
+                echo "Description/Contenu";
+                echo "</th>";
+            echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        foreach ($documents as $document) {
+            $publication = $document->findPublication();
+            echo "<tr>";
+                echo "<td>";  //date("d/m/Y", strtotime($singleNews->findPublication()["dateP"]))
+                echo date("d/m/Y", strtotime($publication['dateP']));
+                echo "</td>";
+                echo "<td>";
+                echo date("h:i:s", strtotime($publication['dateP']));
+                echo "</td>";
+                echo "<td>";
+                echo Utilisateur::findById($publication['idU'])->nomU . " " . Utilisateur::findById($publication['idU'])->prenomU;;
+                echo "</td>";
+                echo "<td>";
+                $typed = $document->idTypeDs()[0];
+                $news_id = TypeD::findByNom("News")->idTypeD;
+                switch($typed) {
+                    case $news_id:
+                        echo $document->nomD;
+                        break;
+                    default:
+                        echo "<a class=\"view-pdf\" href=\"" . $document->urlD . "\" nom=\"" . $document->nomD . "\">" . $document->nomD . "</a>";
+                        break;
+                }
+                echo "</td>";
+                echo "<td>";
+                $typed = $document->idTypeDs()[0];
+                $news_id = TypeD::findByNom("News")->idTypeD;
+                echo "<div style=\"font-size: 0.9em;\">";
+                switch($typed) {
+                    case $news_id:
+                        echo $document->contenuD;
+                        break;
+                    default:
+                        echo $document->descD;
+                        break;
+                }
+                echo "</div>";
+                echo "</td>";                 
+            echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+        echo "</div>";
+    }
+
+    public function futurUsersView() {
+        $inscriptions = Inscription::findAll();
+        echo "<div class=\"table-responsive\">";
+        echo "<table class=\"table table-hover\">";
+        echo "<thead>";
+            echo "<tr>";
+                echo "<th>";
+                echo "Code";
+                echo "</th>";
+                echo "<th>";
+                echo "Validité";
+                echo "</th>";
+                echo "<th>";
+                echo "Type d'Utilisateur";
+                echo "</th>";
+                echo "<th class=\"text-center\">";
+                echo "<img src=\"Image/icon-delete.png\" width=\"25\"/>";
+                echo "</th>";
+            echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        foreach ($inscriptions as $inscription) {
+            echo "<tr>";
+                echo "<td>";
+                echo $inscription->codeI;
+                echo "</td>";
+                echo "<td>";
+                echo $inscription->validiteI;
+                echo "</td>";
+                echo "<td>";
+                echo TypeU::findById($inscription->idTypeU)->nomTypeU;
+                echo "</td>";
+                echo "<td class=\"text-center\">";
+                echo "<form action=\"index.php\" method=\"GET\">";
+                echo "<input type=\"hidden\" name=\"a\" value=\"deleteI\">";
+                echo "<input type=\"hidden\" name=\"idI\" value=\"" . $inscription->idI . "\">";
+                echo "<button type=\"submit\" class=\"btn btn-danger\">Supprimer</button>";
+                echo "</form>";
+                echo "</td>";
+            echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+    }
+
     public static function documentView($document) {
         echo "<div id=\"document\" class=\"col-xs-6 col-sm-3 placeholder test\">";
         echo "<a class=\"view-pdf\" href=\"" . $document->urlD . "\" nom=\"" . $document->nomD . "\">";
@@ -63,7 +177,7 @@ class DocumentsView {
             $corbeille_id = $typeCorbeille->idTypeD;
             echo "<form style=\"display:inline; position: absolute; right: 0;\" action=\"index.php\" method=\"GET\">";
             echo "<input type=\"hidden\" name=\"a\" value=\"deleteD\">";
-            echo "<input type=\"hidden\" name=\"idC\" value=\"" . $corbeille_id . "\">";            
+            echo "<input type=\"hidden\" name=\"idC\" value=\"" . $corbeille_id . "\">";
             echo "<input type=\"hidden\" name=\"idD\" value=\"" . $singleNews->idD . "\">";
             echo "<button type=\"submit\" class=\"btn btn-danger btn-xs\">Supprimer</button>";
 
